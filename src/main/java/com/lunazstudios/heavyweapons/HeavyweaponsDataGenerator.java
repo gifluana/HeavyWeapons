d4a47c754605b930a996188d71b2ca6629752ec3
gifluana <gif.luana@outlook.com>
2024-06-15 06:21:00 -0300

    init commit

---
+++ src/main/java/com/lunazstudios/heavyweapons/HeavyweaponsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.lunazstudios.heavyweapons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HeavyweaponsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
